1127db11197bc627fb337fb342eb18129c82f7b2
ksenia-nesytova <alleheldentot@gmail.com>
2021-08-17 16:44:53 +0700

    injected sorting service into filtered-users

--- src/app/users/filtered-users/filtered-users.component.ts
+++ src/app/users/filtered-users/filtered-users.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SortingService } from "../../sorting.service";
+import { UserService } from "../../user.service";
 @Component({
   selector: 'app-filtered-users',
   templateUrl: './filtered-users.component.html',
-  styleUrls: ['./filtered-users.component.css']
+  styleUrls: ['./filtered-users.component.css'],
+  providers: [ SortingService, UserService],
 })
 export class FilteredUsersComponent implements OnInit {
 
   filteredUsers: any[] = [];
-  
-  constructor() { }
+
+  constructor(private sortingService: SortingService) { }
 
   ngOnInit(): void {
   }
 
+  sort(prop: string) {
+    this.sortingService.sort(this.filteredUsers, prop);
+  }
 }
